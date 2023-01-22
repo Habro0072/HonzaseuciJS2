f57b232b09fa8524a246f3031ed4c7f1e59d27a4
Habro0072 <121198552+Habro0072@users.noreply.github.com>
2023-01-22 13:04:33 +0100

    Add files via upload

---
+++ honzaseuciJS2/tools/index.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    document.getElementById("demo").innerHTML = "změna barvy.";
+    
+    //document.write("došlo ke změně barvy");
+
+    alert("došlo ke změně barvy");
+}
+function okno(){
+    //window.location = "C:/Users/jhn/Desktop/web/web/okno/index.html";
+}
